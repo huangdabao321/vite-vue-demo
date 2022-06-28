73bf57baf2fb5d86bdef8a2cf77a14d8d4526e5b
baoge <huangdabao321@gmail.com>
2022-06-28 19:43:40 +0800

    local

--- src/permission.js
+++ src/permission.js
@@ -1,32 +1,33 @@
 import router from "@/router";
-import { TOKEN } from '@/config/type'
-import { getItem } from '@/utils/storage'
+import { TOKEN } from "@/config/type";
+import { getItem } from "@/utils/storage";
 import { setDocumentTitle } from "@/utils/tools";
 
-const whiteList = ['login']
-const LOGINPATH = '/user/login'
-const defaultRootPath = '/welcome'
-const token = getItem(TOKEN)
-console.log('token', token)
+const whiteList = ["login"];
+const LOGINPATH = "/user/login";
+const defaultRootPath = "/welcome";
+const token = getItem(TOKEN);
 
 router.beforeEach((to, from, next) => {
   // 设置title
-  to.meta && typeof to.meta.title !== 'undefined' && setDocumentTitle(to.meta.title)
+  to.meta &&
+    typeof to.meta.title !== "undefined" &&
+    setDocumentTitle(to.meta.title);
   if (token) {
     if (to.fullPath === LOGINPATH) {
-      next({ path: defaultRootPath})
+      next({ path: defaultRootPath });
     } else {
-      next()
+      next();
     }
   } else {
     if (whiteList.includes(to.name)) {
-      next()
+      next();
     } else {
-      next({ path: LOGINPATH, query: { redirect: to.fullPath } })
+      next({ path: LOGINPATH, query: { redirect: to.fullPath } });
     }
   }
-})
+});
 
-router.afterEach(() =>{
-  console.log('done')
-})
+router.afterEach(() => {
+  console.log("done");
+});
